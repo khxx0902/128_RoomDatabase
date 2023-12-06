d8366950f47d3b2a56f731f849d1f70f09ceac61
mkhaidar168@gmail.com <mkhaidar168@gmail.com>
2023-12-06 21:07:16 +0700

    function to validate input

--- app/src/main/java/com/example/praktikum7/model/EntryViewModel.kt
+++ app/src/main/java/com/example/praktikum7/model/EntryViewModel.kt
@@ -5,7 +5,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.example.praktikum7.repositori.RepositoriSiswa
 
-class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
 
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+}
