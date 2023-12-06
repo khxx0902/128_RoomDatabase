4bb783bc88f8c7874176c8292ffef33e45075229
mkhaidar168@gmail.com <mkhaidar168@gmail.com>
2023-12-06 21:20:17 +0700

    The extension function queries for the [Application] object and returns an instance of [AplikasiSiswa]

--- app/src/main/java/com/example/praktikum7/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/praktikum7/model/PenyediaViewModel.kt
@@ -1,7 +1,10 @@
 package com.example.praktikum7.model
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.praktikum7.AplikasiSiswa
 
 object PenyediaViewModel {
     val Factory =  viewModelFactory {
@@ -13,3 +16,5 @@ object PenyediaViewModel {
         }
     }
 }
+fun CreationExtras.AplikasiSiswa(): AplikasiSiswa = (this
+    [ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
