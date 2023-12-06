b07b7832f93fe7e5116a19c8a6f3f6d0bc53a47a
mkhaidar168@gmail.com <mkhaidar168@gmail.com>
2023-12-06 21:05:51 +0700

    contains the current status of the student

--- app/src/main/java/com/example/praktikum7/model/EntryViewModel.kt
+++ app/src/main/java/com/example/praktikum7/model/EntryViewModel.kt
@@ -1,4 +1,11 @@
 package com.example.praktikum7.model
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.example.praktikum7.repositori.RepositoriSiswa
 
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
 
