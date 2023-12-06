6297ac8cd5c39463ccef060df178ea693f8dc9a9
mkhaidar168@gmail.com <mkhaidar168@gmail.com>
2023-12-06 18:24:27 +0700

    Add Room Database

--- app/src/main/java/com/example/praktikum7/data/DatabaseSiswa.kt
+++ app/src/main/java/com/example/praktikum7/data/DatabaseSiswa.kt
@@ -1,5 +1,25 @@
 package com.example.praktikum7.data
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
 class DatabaseSiswa {
 
+    @Database(entities = [Siswa::class], version = 1, exportSchema = false)
+    abstract class DatabaseSiswa : RoomDatabase() {
+        abstract fun SiswaDao() : SiswaDao
+
+        companion object{
+            @Volatile
+            private var Instance: DatabaseSiswa? = null
+
+            fun getDatabase(context: Context): DatabaseSiswa{
+                return (Instance?: synchronized(this){
+                    Room.databaseBuilder(context, DatabaseSiswa::class.java, "siswa_database").build().also { Instance=it }
+                })
+            }
+        }
+    }
 }
