100bdc693482f6b8850f807071c5f94359c90691
mkhaidar168@gmail.com <mkhaidar168@gmail.com>
2023-12-06 21:18:08 +0700

    add object PenyediaViewModel

--- app/src/main/java/com/example/praktikum7/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/praktikum7/model/PenyediaViewModel.kt
@@ -1,5 +1,15 @@
 package com.example.praktikum7.model
 
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 
-
-
+object PenyediaViewModel {
+    val Factory =  viewModelFactory {
+        initializer {
+            HomeViewModel(AplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EntryViewModel(AplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
